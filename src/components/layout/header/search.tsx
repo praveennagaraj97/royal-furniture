f73840acd9ad974393a5388c78291d402d46e108
praveenNagaraj97 <praveennagaraj597@gmail.com>
2025-12-16 12:41:34 +0530

    Update SearchBar component to use rounded-xl styling for improved aesthetics

--- src/components/layout/header/search.tsx
+++ src/components/layout/header/search.tsx
@@ -8,7 +8,7 @@ const SearchBar: FC = () => {
       <input
         type="text"
         placeholder="What you are looking for?"
-        className="bg-[#F9FAFB] py-3 w-full rounded-lg placeholder:text-[#9CA3AF] pl-9 border
+        className="bg-[#F9FAFB] py-3 w-full rounded-xl placeholder:text-[#9CA3AF] pl-9 border
         hover:border-gray-400/60 focus:border-gray-400/60 focus:outline-none
         transition-colors duration-150 delay-75 border-gray-100"
         suppressHydrationWarning
