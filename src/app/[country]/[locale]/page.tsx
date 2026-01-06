a1fc5cb8168e50ad51fb66e7612681bc2ef252a4
praveenNagaraj97 <praveennagaraj597@gmail.com>
2026-01-06 20:37:24 +0530

    patch

--- src/app/[country]/[locale]/page.tsx
+++ src/app/[country]/[locale]/page.tsx
@@ -6,8 +6,10 @@ import MegaPriceDropBanner from '@/components/home/mega-price-drop-banner';
 import PromotionalBanner from '@/components/home/promotional-banner';
 import PromotionalOffers from '@/components/home/promotional-offers';
 import SofaAndSeating from '@/components/home/sofa-and-seating';
+import CountdownTag from '@/components/shared/ui/countdown-tag';
 import ProductListing from '@/components/shared/ui/product-listing';
 import { ecommerceService } from '@/services/api/ecommerce-service';
+import { productsData } from '@/temp/data/products-data';
 import { getCountriesWithLocaleParams } from '@/utils/generated';
 import { NextPage } from 'next';
 
@@ -23,16 +25,13 @@ const Home: NextPage = async () => {
       )}
       <PromotionalOffers offers={data.offer_banners || []} />
       {data.trending_products && (
-        <ProductListing
-          title="Trending Products"
-          products={data.trending_products.items || []}
-        />
+        <ProductListing title="Trending Products" products={productsData} />
       )}
       <MegaPriceDropBanner />
       <SofaAndSeating />
       <FurnitureShowcase />
 
-      {/* <ProductListing
+      <ProductListing
         title={
           <div className="flex gap-1.5 flex-wrap">
             <span className="lg:text-2xl md:text-xl text-lg text-indigo-slate">
@@ -41,9 +40,8 @@ const Home: NextPage = async () => {
             <CountdownTag hours={12} minutes={30} seconds={0} />
           </div>
         }
-        seeAllHref="/products"
         products={productsData}
-      /> */}
+      />
 
       <PromotionalBanner />
 
@@ -52,11 +50,7 @@ const Home: NextPage = async () => {
       <HouseHolds />
       <PromotionalBanner />
       <FurnitureShowcase />
-      {/* <ProductListing
-        title="New Arrivals"
-        seeAllHref="/products"
-        products={productsData}
-      /> */}
+      <ProductListing title="New Arrivals" products={productsData} />
 
       <PromotionalBanner />
       <LatestBlogs />
