8e81de91c607c9da87914819b7d6ac91279226d2
praveenNagaraj97 <praveennagaraj597@gmail.com>
2026-01-09 19:34:26 +0530

    refactor: update CategoryNav component to use custom useAppPathName hook for pathname management and adjust styling for improved layout

--- src/components/layout/categories/category-nav.tsx
+++ src/components/layout/categories/category-nav.tsx
@@ -1,10 +1,9 @@
 'use client';
 
 import { StaggerContainer, StaggerItem } from '@/components/shared/animations';
-import { AppLink } from '@/hooks';
+import { AppLink, useAppPathName } from '@/hooks';
 import { CategoryItem } from '@/types';
 import { useTranslations } from 'next-intl';
-import { usePathname } from 'next/navigation';
 import { FC } from 'react';
 
 interface CategoryNavProps {
@@ -12,24 +11,24 @@ interface CategoryNavProps {
 }
 
 const CategoryNav: FC<CategoryNavProps> = ({ categories }) => {
-  const pathName = usePathname();
+  const pathName = useAppPathName();
   const t = useTranslations('header');
 
   return (
     <div className="shadow-md relative z-40">
-      <nav className="container mx-auto xl:px-12 lg:px-10 md:px-6 sm:px-4 px-3 ">
+      <nav className="container mx-auto xl:px-12 lg:px-10 md:px-6 sm:px-4 px-3 max-w-5xl flex justify-center">
         {/* Mobile: horizontal scroll, Desktop: centered wrap */}
         <StaggerContainer
           mode="animate"
           staggerChildren={0.05}
           delayChildren={0}
           duration={0.3}
-          className="flex items-center gap-2 sm:gap-3 overflow-x-auto no-scrollbar sm:justify-center sm:flex-wrap py-3"
+          className="flex items-center gap-2 sm:gap-3 overflow-x-auto py-3 "
         >
           <AppLink
             href="/"
             className={`whitespace-nowrap px-4 py-2 rounded-full text-xs sm:text-sm font-medium transition-colors block hover:scale-105 active:scale-95 ${
-              pathName === '/products'
+              pathName === '/'
                 ? 'bg-deep-maroon text-white'
                 : 'bg-gray-100 text-gray-700 hover:bg-gray-200'
             }`}
