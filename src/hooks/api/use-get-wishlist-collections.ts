fded6b6ceeb86ab600d122402a6b850ef3dd2b89
praveenNagaraj97 <praveennagaraj597@gmail.com>
2026-01-16 15:52:32 +0530

    feat: conditionally fetch wishlist collections based on user authentication status

--- src/hooks/api/use-get-wishlist-collections.ts
+++ src/hooks/api/use-get-wishlist-collections.ts
@@ -3,12 +3,15 @@
 import { apiFetcher } from '@/config/axios';
 import { API_ROUTES } from '@/constants/api-routes';
 import type { WishlistCollectionsResponse } from '@/types/response';
+import { useAuth } from '@/contexts/auth-context';
 import useSWR from 'swr';
 
 export const useGetWishlistCollections = () => {
+  const { isAuthenticated } = useAuth();
+  
   const { data, error, isLoading, mutate } =
     useSWR<WishlistCollectionsResponse>(
-      API_ROUTES.WISHLIST.COLLECTIONS,
+      isAuthenticated ? API_ROUTES.WISHLIST.COLLECTIONS : null,
       apiFetcher<WishlistCollectionsResponse>,
       {
         revalidateOnFocus: false,
