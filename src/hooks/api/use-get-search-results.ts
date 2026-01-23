82af7a559d27ad2f01e5b6e54bf55a921cada14b
Praveen Nagaraj <praveennagaraj597@gmail.com>
2026-01-23 16:17:21 +0530

    patch

--- src/hooks/api/use-get-search-results.ts
+++ src/hooks/api/use-get-search-results.ts
@@ -42,7 +42,7 @@ export const useGetSearchResults = ({
     {
       revalidateOnFocus: false,
       revalidateOnReconnect: false,
-    }
+    },
   );
 
   const products = data?.data?.results || [];
