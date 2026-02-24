d3db0e0c8de202f1d6924b4d5bcb50227f1ef4a3
Praveen Nagaraj <praveennagaraj597@gmail.com>
2026-02-24 20:05:15 +0530

    feat: conditionally render PromosCard based on authentication status

--- src/components/checkout/common/info-sections/index.tsx
+++ src/components/checkout/common/info-sections/index.tsx
@@ -1,14 +1,17 @@
 'use client';
 
+import { useAuth } from '@/contexts/auth-context';
 import { FC } from 'react';
 import { PromosCard } from './promos-card';
 import { SupportCard } from './support-card';
 
 export const CartInfoSections: FC = () => {
+  const { isAuthenticated } = useAuth();
+
   return (
     <div className="space-y-4">
       {/* <DeliveryInfoCard /> */}
-      <PromosCard />
+      {isAuthenticated ? <PromosCard /> : null}
       <SupportCard />
     </div>
   );
